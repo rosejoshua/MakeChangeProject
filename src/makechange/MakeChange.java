37f0d5d27e5f72bf5a8f157d846f925c4535c713
Joshua Rose <jlr007@protonmail.com>
2021-01-16 14:55:40 -0500

    Started getChange method

--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -26,7 +26,30 @@ public class MakeChange {
 			purchPrompt();
 		}
 		else if (price < paid) {
-//			getchange(String price, String paid);
+			getChange(price, paid);
+		}
+	}
+	
+	public static void getChange(double price, double paid) {
+				
+		int centsPrice = (int)(price*100);
+		System.out.println(centsPrice);
+		int centsPaid = (int)(paid*100);
+		System.out.println(centsPaid);
+		int change = centsPaid - centsPrice;
+		System.out.println(change);
+		
+		System.out.print("Change to be given is $" + (change/100) + ".");
+		System.out.printf("%02d", (change%100));
+		System.out.println(" in the following denominations:");
+		
+		if (change>=2000) {
+			int numOfTwenty = change/2000;
+			change = change % 2000;
+			System.out.print("(" + numOfTwenty + ")" + " 20 dollar bill");
+			if (numOfTwenty > 1) {System.out.print("s");
+			System.out.println();
+			}
 		}
 	}
 
