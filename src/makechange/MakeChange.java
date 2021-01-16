594da9b1013155477dc0c70efec5283243253464
Joshua Rose <jlr007@protonmail.com>
2021-01-16 13:54:28 -0500

    Created first method

--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -1,8 +1,33 @@
 package makechange;
 
+import java.util.Scanner;
+
 public class MakeChange {
+	static Scanner input = new Scanner(System.in);
 	public static void main(String[] args) {
 		
+		purchPrompt();		
+	}
+	
+	public static void purchPrompt() {
+		System.out.println("Enter price of item: ");
+		double price = input.nextDouble();
+		input.nextLine();
+		
+		System.out.println("Enter amount paid: ");
+		double paid = input.nextDouble();
+		input.nextLine();
+		
+		if (price == paid) {
+			System.out.println("Transaction complete. No change required.");
+		}
+		else if (price > paid) {
+			System.out.println("Not enough paid, try again: ");
+			purchPrompt();
+		}
+		else if (price < paid) {
+//			getchange(String price, String paid);
+		}
 	}
 
 }
